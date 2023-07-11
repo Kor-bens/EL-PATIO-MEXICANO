df8b33975af25561af238035b9bd01bbfaa09688
Gikujo <miguel.segura@outlook.fr>
2023-07-11 14:43:33 +0200

    navbar maj

--- public/composantJs/fonctionsGeneriques.js
+++ public/composantJs/fonctionsGeneriques.js
@@ -6,3 +6,21 @@ let toutesLesImages = document.querySelectorAll('img');
 toutesLesImages.forEach(image => {
     image.setAttribute('loading', 'lazy');
 });
+
+// --- SOULIGNER UN ITEM DU MENU LORS DU SURVOL
+
+let navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(navLink => {
+    if (navLink.name != "menu") {
+        navLink.addEventListener('mouseover', () => {
+            navLink.classList.add('underline');
+
+            navLink.addEventListener('mouseout', () => {
+                navLink.classList.remove('underline');
+            });
+        });
+    } else {
+        console.log(navLink);
+    }
+});
