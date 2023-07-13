0cfc5cca79e407948366e8c5689fabed58af1082
Gikujo <131861784+Gikujo@users.noreply.github.com>
2023-07-13 12:41:44 +0200

    Miguel1307 (#21)
    * bon comportement titre
    
    * meilleur comportement navbar
    
    * texte index responsive + tentatives navbar
    
    * meilleur comportement navbar
    
    * texte index responsive + tentatives navbar
    
    * maj navbar 1307
    
    * maj comportement navbar

--- public/composantJs/navbar.js
+++ public/composantJs/navbar.js
@@ -1,7 +1,7 @@
 // ----- SOULIGNER UN ITEM DU MENU LORS DU SURVOL
 
 let navLinks = document.querySelectorAll('.nav-link');
-
+let dropdown = document.querySelector('.dropdown-menu');
 
 navLinks.forEach(navLink => {
   if (navLink.id != "menu") {
@@ -12,7 +12,6 @@ navLinks.forEach(navLink => {
         navLink.classList.remove('underline');
       });
     });
-  } else {
   }
 });
 
@@ -21,13 +20,6 @@ navLinks.forEach(navLink => {
 // -- On récupère les variables
 let prevScrollPos = window.scrollY;
 const navbar = document.querySelector('#navbar');
-
-let screenWidth = parseInt(window.outerWidth);
-
-// -- Toutes les 100ms, on vérifie la valeur du scroll
-setInterval(() => {
-  screenWidth = parseInt(window.outerWidth);
-}, 100);
 
 // -- Lors du chargement de la page, si on est en haut de la page, la navbar s'affiche
 window.addEventListener('load', () => {
@@ -50,7 +42,6 @@ window.onscroll = function () {
 
     // Si on descend, on la cache
   } else {
-    // console.log(`prevPos : ${prevScrollPos}, currentPos : ${currentScrollPos}`);
     navbar.classList.add('hidden');
     navbar.classList.remove('shown');
   }
@@ -60,69 +51,69 @@ window.onscroll = function () {
 
 // ----- GÉRER L'ANIMATION D'APPARITION DU MENU DÉROULANT LORS DU SURVOL
 
+// -- Variable qui va s'alterner afin de ne faire qu'un seul addEventListener
 let isDone = false;
+
+// -- Action réalisée lorsque les conditions du dropdown sont valides
+function action() {
+  const dropdown = document.querySelector('.dropdown-menu');
+  dropdown.classList.add('show', 'shown');
+}
+
+// -- Récupérer la position du curseur et la comparer à celle du dropdown pour le cacher si on en sort
+function getPosition(e) {
+  if (dropdown.classList.contains('show')) {
+
+    // On soustrait le scrollY car la position du curseur change en fonction du scroll, mais pas celle du menu.
+    // Si on omet de soustraire le scrollY, la règle de "curseur hors dropdown" sera considérée comme toujours vraie.
+    var x = e.pageX;
+    var y = e.pageY - window.scrollY;
+
+    // On récupère les coordonnées du menu dropdown pour les comparer
+    let ddCoordonnees = dropdown.getBoundingClientRect();
+    // Ainsi que celles du menu pour en soustraire la height et ne pas fausser la mesure
+    let menuCoordonnees = menu.getBoundingClientRect();
+
+    // Condition pour comparer la position du curseur à celle du menu dropdown - Si en dehors, on cache le dropdown
+    if (x < ddCoordonnees.left || x > ddCoordonnees.right || y < ddCoordonnees.top - menuCoordonnees.height || y > ddCoordonnees.bottom) {
+      dropdown.classList.remove('show', 'shown');
+      dropdown.classList.add('hidden');
+    }
+  }
+}
+
+// -- Fonction pour que le bouton "menu" amène vers la page "menu.php"
+function handleMenuClick() {
+  window.location.href = 'menu.php';
+}
+
+// Fonction principale - ajout et enlèvement des écouteurs d'évènements en fonction de la screenWidth
 function gererAnimation() {
 
   let menu = document.querySelector('#menu');
-  let dropdown = document.querySelector('.dropdown-menu');
   var body = document.body;
-
-  // -- Action réalisée lorsque les conditions du dropdown sont valides
-  const action = () => {
-    dropdown.classList.add('show', 'shown');
-  };
-
-  // -- Récupérer la position du curseur et la comparer à celle du dropdown pour le cacher si on en sort
-  function getPosition(e) {
-    if (dropdown.classList.contains('show')) {
-
-      // On soustrait le scrollY car la position du curseur change en fonction du scroll, mais pas celle du menu.
-      // Si on omet de soustraire le scrollY, la règle de "curseur hors dropdown" sera considérée comme toujours vraie.
-      var x = e.pageX;
-      var y = e.pageY - window.scrollY;
-
-      // On récupère les coordonnées du menu dropdown pour les comparer
-      let ddCoordonnees = dropdown.getBoundingClientRect();
-      // Ainsi que celles du menu pour en soustraire la height et ne pas fausser la mesure
-      let menuCoordonnees = menu.getBoundingClientRect();
-
-      // Condition pour comparer la position du curseur à celle du menu dropdown - Si en dehors, on cache le dropdown
-      if (x < ddCoordonnees.left || x > ddCoordonnees.right || y < ddCoordonnees.top - menuCoordonnees.height || y > ddCoordonnees.bottom) {
-        dropdown.classList.remove('show', 'shown');
-        dropdown.classList.add('hidden');
-      }
-    }
-  }
+  let screenWidth = window.innerWidth;
 
   if (screenWidth > 991) {
     if (!isDone) {
-
       menu.addEventListener('mouseover', action);
       body.addEventListener("mousemove", getPosition);
-      menu.addEventListener('click', (e) => {
-        window.location.href = 'menu.php';
-      });
+      menu.addEventListener('click', handleMenuClick);
       isDone = true;
     }
 
 
   } else {
+
     if (isDone) {
-      console.log(`screenWidth : ${screenWidth}`);
       menu.removeEventListener('mouseover', action);
-      menu.removeEventListener('click', (e) => {
-        window.location.href = 'menu.php';
-      });
       body.removeEventListener("mousemove", getPosition);
-      if (dropdown.classList.contains('show')) {
-        dropdown.classList.remove('show');
-      }
-      if (dropdown.classList.contains('shown')) {
-        dropdown.classList.remove('shown');
-      }
+      menu.removeEventListener('click', handleMenuClick);
+      isDone = false;
     }
   }
 }
 
+// -- On va appeler la fonction principale sur chargement de la page et sur resizing
 window.addEventListener('load', gererAnimation);
 window.addEventListener('resize', gererAnimation); //-- N'est pas utilisé car lorsqu'on ajoute les eventListeners sur un resize supérieur à 991px, ça en ajoute beaucoup d'un coup, alors que lorsque le resize est inférieur ) 991px, ça n'en enlève qu'un seul à la fois, c'est donc inefficace.
