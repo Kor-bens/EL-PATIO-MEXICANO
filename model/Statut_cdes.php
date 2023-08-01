8a022696d14f4fffe67a574966a88c450df01cb7
Tortosa <sofianetortosa@gmail.com>
2023-08-01 08:28:15 +0200

    Class

---
+++ model/Statut_cdes.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Statut_cdes{
+    private int $id;
+    private string $nom;
+    
+
+    public function __construct($id,$nom){
+        $this->id = $id;
+        $this->nom = $nom;
+    }
+}
