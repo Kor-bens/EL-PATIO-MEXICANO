96f5fae1f16adde0ba9e490fb9bd758c369182ae
Gikujo <131861784+Gikujo@users.noreply.github.com>
2023-08-01 11:24:18 +0200

    nouvelles classes (#32)

--- src/model/Categorie_msg.php
+++ src/model/Categorie_msg.php
@@ -1,25 +1,29 @@
 <?php 
 
 class Categorie_msg{
-    private int $id_categorie_msg;
-    private string $nom_categorie;
+    private int     $id_categorie_msg;
+    private string  $nom_categorie;
     
     public function __construct($id_categorie_msg,$nom_categorie){
         $this->id_categorie_msg = $id_categorie_msg;
-        $this->nom_categorie = $nom_categorie;
+        $this->nom_categorie    = $nom_categorie;
        
     }
 
     public function getIdCategorieMsg(): int {
+
         return $this->id_categorie_msg;
     }
+
     public function setIdCategorieMsg(int $id_categorie_msg) {
         $this->id_categorie_msg = $id_categorie_msg;
     }
 
     public function getNomCategorie(): string {
+
         return $this->nom_categorie;
     }
+
     public function setNomCategorie(string $nom_categorie) {
         $this->nom_categorie = $nom_categorie;
     }
