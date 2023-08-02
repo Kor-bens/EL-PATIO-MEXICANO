4737d5bf0e052d682b5e8b72df8c5762099ecb78
tortosa <tortosa.sofiane@hotmail.fr>
2023-08-02 15:05:12 +0200

    maj (#35)

--- src/dao/Requete.php
+++ src/dao/Requete.php
@@ -3,6 +3,8 @@
 class Requete
 {
     public Const LISTE_DEMANDES = 'SELECT id, nom_categorie FROM categories_msg order by nom_categorie';
-    public Const REQ_INS_MESS = 'INSERT INTO message (id, nom_categorie,) VALUE ()';
+    public Const REQ_INS_MESS = 'INSERT INTO messages(texte) VALUE (?)';
+    public const REQ_ROLE_USER = 'SELECT id, nom, prenom, mail, telephone, adresse, id_role FROM personnes';
+    
 }
 
