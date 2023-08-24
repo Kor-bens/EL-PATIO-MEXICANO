1af7e98c070a4af7ac7c6bcef3fb8e71cd89314d
Gikujo <131861784+Gikujo@users.noreply.github.com>
2023-08-24 13:46:12 +0200

    nouveau formulaire inscription-connexion (#47)

--- src/views/inscription.php
+++ src/views/inscription.php
@@ -1,7 +1,7 @@
 <?php require_once("common/header.php") ;
     require_once 'src/controllers/Message.php';
 ?>
-<link rel="stylesheet" href="../../assets/css/contact.css">
+<link rel="stylesheet" href="../../assets/css/inscription.css">
 </head>
 
 <body>
@@ -18,41 +18,52 @@
             <div class="alert alert-danger" role="alert"><?= $err ?></div>
     <?php }
     } ?>
-    <div class="container-img-description">
-        <div id="container-bloc" class="mtb-4">
-            <div class="bloc-image"></div>
-            <div class="bloc-formulaire un orange" id="connexion-inscription">
-                <h2 id="titre-formulaire" class="fst-2">Connexion</h2>
-                <form id="formulaire" action="/inscription" method="post">
-
-                    <label class="fsp-3 text-center text-center" for="nom">Nom :</label>
-                    <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3 text-center" for="prenom">Prénom :</label>
-                    <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom" data-sb-validations="required" class="vert mb-2 fsp-3">                    
-
-                    <label class="fsp-3" for="email">Email :</label>
-                    <input type="email" id="email" name="email" placeholder="Entrez votre email" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3" for="email-confirm">Confirmez votre email :</label>
-                    <input type="email" id="email-confirm" name="email-confirm" placeholder="Entrez votre email" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3" for="mdp">Mot de passe :</label>
-                    <input type="password" id="mdp" name="mdp" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3" for="mdp-confirm">Confirmez votre mot de passe :</label>
-                    <input type="password" id="mdp-confirm" name="mdp-confirm" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3" for="telephone">Numéro de téléphone :</label>
-                    <input type="phone" id="telephone" name="telephone" placeholder="Entrez votre numéro de téléphone" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <label class="fsp-3" for="adresse">Adresse :</label>
-                    <input type="adress" id="adresse" name="adresse" placeholder="Entrez votre adresse" data-sb-validations="required" class="vert mb-2 fsp-3">
-
-                    <input id="btn" type="submit" value="Envoyer" class="vert btn mb-1 fsb-3">
-                </form>
-            </div>
+    <div class="bloc-formulaire un orange w-50 mt-5" id="connexion-inscription">
+        
+        <div id="switch" class="row fsp-3 w-100 m-0 p-0">
+            <div id="login" class="col-6 p-3">Connexion</div>
+            <div id="signup" class="col-6 p-3 inactive">Inscription</div>
         </div>
+
+        <form id="signup-form" action="/inscription" method="post" class="d-none">
+
+            <label class="fsp-3 text-center text-center" for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom" placeholder="Entrez votre nom" data-sb-validations="required" class="vert mb-2 fsp-3 text-center">
+
+            <label class="fsp-3 text-center" for="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="prenom" placeholder="Entrez votre prénom" data-sb-validations="required" class="vert mb-2 fsp-3">
+            
+
+            <label class="fsp-3" for="email">Email :</label>
+            <input type="email" id="email" name="email" placeholder="Entrez votre email" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="email-confirm">Confirmez votre email :</label>
+            <input type="email" id="email-confirm" name="email-confirm" placeholder="Entrez votre email" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="mdp">Mot de passe :</label>
+            <input type="password" id="mdp" name="mdp" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="mdp-confirm">Confirmez votre mot de passe :</label>
+            <input type="password" id="mdp-confirm" name="mdp-confirm" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="telephone">Numéro de téléphone :</label>
+            <input type="tel" id="telephone" name="telephone" placeholder="Entrez votre numéro de téléphone" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="adresse">Adresse :</label>
+            <input type="text" id="adresse" name="adresse" placeholder="Entrez votre adresse" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <input id="btn" type="submit" value="Envoyer" class="vert btn mb-1 fsb-3">
+        </form>
+
+        <form id="login-form">
+            <label class="fsp-3" for="email">Email :</label>
+            <input type="email" id="login-email" name="email" placeholder="Entrez votre email" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <label class="fsp-3" for="mdp">Mot de passe :</label>
+            <input type="password" id="login-mdp" name="mdp" data-sb-validations="required" class="vert mb-2 fsp-3">
+
+            <input id="login-btn" type="submit" value="Envoyer" class="vert btn mb-1 fsb-3">
+        </form>
     </div>
 
 
@@ -63,7 +74,7 @@
         <!-- <img id="plats-table" src="../Ressources/plats-table.jpg" alt="plats-table"> -->
     </div>
     <?php require("common/footer.php") ?>
-    <script src="../../assets/composantJs/contact.js"></script>
+    <script src="../../assets/composantJs/inscription.js"></script>
 
 </body>
 
